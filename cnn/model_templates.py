4afff6d46178811d0cc43f21cc39236173008544
sweetchild222 <sweetchild22.ik@gmail.com>
2020-11-16 12:20:15 +0900

    gege

--- cnn/model_templates.py
+++ cnn/model_templates.py
@@ -13,12 +13,12 @@ def template_complex(gradient, input_shape, classes):
 
     layers = [
         {'type':'input', 'parameter':{'input_shape':input_shape}},
-        {'type':'convolution', 'parameter':{'filters':5, 'kernel_size':(3, 3), 'strides':(1, 1), 'padding':True, 'activation':'relu', 'gradient':gradient}},
-        {'type':'convolution', 'parameter':{'filters':5, 'kernel_size':(3, 3), 'strides':(1, 1), 'padding':True, 'activation':'relu', 'gradient':gradient}},
+        {'type':'convolution', 'parameter':{'filters':8, 'kernel_size':(3, 3), 'strides':(1, 1), 'padding':True, 'activation':'relu', 'gradient':gradient}},
+        {'type':'maxPooling', 'parameter':{'pool_size':(2, 2), 'strides':None}},
+        {'type':'convolution', 'parameter':{'filters':8, 'kernel_size':(3, 3), 'strides':(1, 1), 'padding':True, 'activation':'relu', 'gradient':gradient}},
         {'type':'maxPooling', 'parameter':{'pool_size':(2, 2), 'strides':None}},
         {'type':'flatten', 'parameter':{}},
-        {'type':'dense', 'parameter':{'units':256, 'activation':'relu', 'gradient':gradient}},
-        {'type':'dense', 'parameter':{'units':64, 'activation':'relu', 'gradient':gradient}},
+        {'type':'dense', 'parameter':{'units':128, 'activation':'relu', 'gradient':gradient}},
         {'type':'dense', 'parameter':{'units':classes, 'activation':'softmax', 'gradient':gradient}}]
 
     return layers
